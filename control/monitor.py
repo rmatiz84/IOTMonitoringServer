7a288f4a820a6327a24a5c420646159bf2ad541e
rmatiz84 <r.matiz@uniandes.edu.co>
2022-07-17 20:29:39 -0500

    cambios reto

--- control/monitor.py
+++ control/monitor.py
@@ -71,7 +71,7 @@ def analyze_data_reto():
     print("Calculando alertas reto...")
 
     data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(minutes=10))
+        base_time__gte=datetime.now() - timedelta(hours=2))
     aggregation = data.annotate(check_value=Avg('avg_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
