1131dca0437b280daaf9ce944dc10aec157a08f9
rmatiz84 <r.matiz@uniandes.edu.co>
2022-07-17 20:09:22 -0500

    cambio reto

--- control/monitor.py
+++ control/monitor.py
@@ -63,6 +63,57 @@ def analyze_data():
     print(len(aggregation), "dispositivos revisados")
     print(alerts, "alertas enviadas")
 
+def analyze_data_reto():
+    # Consulta todos los datos de la última hora, los agrupa por estación y variable
+    # Compara el promedio con los valores límite que están en la base de datos para esa variable.
+    # Si el promedio se excede de los límites, se envia un mensaje de alerta.
+
+    print("Calculando alertas reto...")
+
+    data = Data.objects.filter(
+        base_time__gte=datetime.now() - timedelta(minutes=1))
+    aggregation = data.annotate(check_value=Avg('avg_value')) \
+        .select_related('station', 'measurement') \
+        .select_related('station__user', 'station__location') \
+        .select_related('station__location__city', 'station__location__state',
+                        'station__location__country') \
+        .values('check_value', 'station__user__username',
+                'measurement__name',
+                'measurement__max_value',
+                'measurement__min_value',
+                'station__location__city__name',
+                'station__location__state__name',
+                'station__location__country__name')
+    alerts = 0
+    for item in aggregation:
+        alert = False
+
+        variable = item["measurement__name"]
+        max_value = item["measurement__max_value"] or 0
+        min_value = item["measurement__min_value"] or 0
+
+        country = item['station__location__country__name']
+        state = item['station__location__state__name']
+        city = item['station__location__city__name']
+        user = item['station__user__username']
+
+        print(item["check_value"], "Check value")
+        print(max_value, "Max value")
+        print(min_value, "Min value")
+
+        if item["check_value"] > max_value or item["check_value"] < min_value:
+            alert = True
+
+        if alert:
+            message = "ALERT RETO {} ".format(variable)
+            print(message, "Mensaje de alerta reto")
+            topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
+            print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+            client.publish(topic, message)
+            alerts += 1
+
+    print(len(aggregation), "dispositivos revisados reto")
+    print(alerts, "alertas enviadas reto")
 
 def on_connect(client, userdata, flags, rc):
     '''
@@ -111,6 +162,7 @@ def start_cron():
     '''
     print("Iniciando cron...")
     schedule.every(5).minutes.do(analyze_data)
+    schedule.every(1).minutes.do(analyze_data_reto)
     print("Servicio de control iniciado")
     while 1:
         schedule.run_pending()
